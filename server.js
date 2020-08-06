7e6feac72fc0fce36d71a64a4bf176b6c7d4eacd
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-08-06 16:14:16 -0500

    listening

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 app.use(express.static("public"));
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +17,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
+
 
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
